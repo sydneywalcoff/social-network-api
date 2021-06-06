4ed70c252ac0300db23a7aaf7f098b89565ad2aa
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-05 18:01:52 -0700

    add getAllUsers and getUserById fxns to controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,42 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with that id!' });
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+};
+
+module.exports = userController;
